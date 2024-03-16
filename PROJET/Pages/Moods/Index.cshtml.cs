da917ee5b4a0b5a6b3a015bbcdc8a5d0b8c8aa25
Jatiw0u <maxime.ducornet@etud.u-picardie.fr>
2024-03-17 00:25:54 +0100

    Mood_Final_V2

--- PROJET/Pages/Moods/Index.cshtml.cs
+++ PROJET/Pages/Moods/Index.cshtml.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.EntityFrameworkCore;
@@ -11,6 +12,7 @@ using PROJET.ViewModels;
 
 namespace PROJET.Pages.Moods
 {
+    [Authorize(Roles = "ADMINISTRATEUR")]
     public class IndexModel : PageModel
     {
         private readonly PROJET.Data.ApplicationDbContext _context;
--- PROJET/Pages/RefMoods/Index.cshtml.cs
+++ PROJET/Pages/RefMoods/Index.cshtml.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.EntityFrameworkCore;
@@ -10,6 +11,7 @@ using PROJET.Model;
 
 namespace PROJET.Pages.RefMoods
 {
+    [Authorize(Roles = "ADMINISTRATEUR")]
     public class IndexModel : PageModel
     {
         private readonly PROJET.Data.ApplicationDbContext _context;
